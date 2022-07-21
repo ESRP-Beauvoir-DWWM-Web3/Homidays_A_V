b7154cada7fa99235ca04fc79b51f4142e61b34c
VanWEB11 <kivouvouvan@gmail.com>
2022-07-21 15:22:21 +0200

    Affichage reussi selon role

--- src/Controller/ProfileController.php
+++ src/Controller/ProfileController.php
@@ -1,7 +1,8 @@
 <?php
 
 namespace App\Controller;
-
+use App\Entity\User;
+use App\Repository\UserRepository;
 use App\Controller\ProfileController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -14,11 +15,13 @@ class ProfileController extends AbstractController
      
      * @Route("/profile", name="app_profile")
      */
+    
 
-
+  
+   
     public function index(): Response
     {            
-       
+        // $notifications
         return $this->render('profile/index.html.twig'); 
             // ['controller_name' => 'ProfileController',]
 
