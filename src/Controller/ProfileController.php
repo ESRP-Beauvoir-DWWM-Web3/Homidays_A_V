869e7107f0d3239c1ff201e566ae7810070ee8c7
VanWEB11 <kivouvouvan@gmail.com>
2022-07-21 11:27:20 +0200

    Affichage profil en fonction du role user

--- src/Controller/ProfileController.php
+++ src/Controller/ProfileController.php
@@ -2,24 +2,27 @@
 
 namespace App\Controller;
 
+use App\Controller\ProfileController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Security\Http\Authentication\UserAuthenticatorInterface;
 
-class VacancierProfileController extends AbstractController
+class ProfileController extends AbstractController
 {
     /**
-     * @Route("/vacancier/profile", name="app_vacancier_profile")
+     
+     * @Route("/profile", name="app_profile")
      */
+
+
     public function index(): Response
     {            
        
-        return $this->render('vacancier_profile/index.html.twig'); 
-            // ['controller_name' => 'VacancierProfileController',]
+        return $this->render('profile/index.html.twig'); 
+            // ['controller_name' => 'ProfileController',]
 
     }
     
     
-    // $user->setRoles(['ROLES_VACANCIER']);
 }
