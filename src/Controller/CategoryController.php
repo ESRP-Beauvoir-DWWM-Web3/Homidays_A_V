28c4a4e6b6ce954c756a92640694b1cafa8f14c8
Arielle243 <nionguinzaba.arielle@gmail.com>
2022-07-26 16:41:04 +0200

    modification categories

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -4,11 +4,12 @@ namespace App\Controller;
 
 use App\Entity\Category;
 use App\Form\CategoryType;
+use App\Controller\CategoryController;
 use App\Repository\CategoryRepository;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 /**
  * @Route("/category")
